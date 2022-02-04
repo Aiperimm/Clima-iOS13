95435e2c1b884381127fc317ef81ceb69bd9a536
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-02-03 20:01:07 -0500

    we added background dark and light mood, and ui text view

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate{ // manage and edtitig of text field object
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self// text friel should report back to ViewContrl
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // <-- this code will tell the seacrh we are done with editing!and u can dissmiss the keyboard.
+      
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {  // <-- text field will say to ViewContrl, the user pressed the return key in the keyboard..
+        searchTextField.endEditing(true) // <-- this code will tell the seacrh we are done with editing!and u can dissmiss the keyboard.
+        
+        return true  // we have to returnn true because this func expects a boolean output
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something" // its reminding user to type here
+            return false
+        }
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) { // <-- this code says to ViewContrl that user stopped editing
+        //  use searchTextField.text to get weather for that city.
+        
+        searchTextField.text = ""
+    }
+    
+    
 }
 
